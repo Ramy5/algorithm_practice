e8c578e7dc84f7a9dc798ba4e759c38fcaef36f4
Ramy <ramyalsabry.dev@gmail.com>
2024-04-15 22:20:43 +0200

    selection sort

--- 4-Sort/selection-sort/main.js
+++ 4-Sort/selection-sort/main.js
@@ -1,17 +1,13 @@
 const selectionSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    let elementIndex = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
 
     for (let k = i + 1; k < arr.length; k++) {
-      console.log(arr[elementIndex], arr[k]);
-      if (arr[elementIndex] > arr[k]) {
-        console.log("inside condition", arr[elementIndex], arr[k]);
-        [arr[elementIndex], arr[k]] = [arr[k], arr[elementIndex]];
-        console.log(arr);
-        elementIndex = k;
-      } else {
-        elementIndex = i;
-      }
+      if (arr[minIndex] > arr[k]) minIndex = k;
+    }
+
+    if (minIndex !== i) {
+      [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
     }
   }
 
@@ -19,3 +15,7 @@ const selectionSort = (arr) => {
 };
 
 console.log(selectionSort([6, 3, 1, 4, 2, 90, 10])); // [1, 2, 3, 4, 6, 10, 90]
+// [1, 3, 6, 4, 2, 90, 10]
+// [1, 2, 6, 4, 3, 90, 10]
+// [1, 2, 3, 4, 6, 90, 10]
+// [1, 2, 3, 4, 6, 10, 90]
