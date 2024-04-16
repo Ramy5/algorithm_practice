5e3b64b46540eb738a36cf5a1032d6cf1c7207a0
ramy sabry <ramyalsabry.dev@gmail.com>
2024-04-16 16:55:02 +0200

    selection sort by python

--- 4-Sort/selection-sort/main.js
+++ 4-Sort/selection-sort/main.js
@@ -1,5 +1,27 @@
+// const selectionSort = (arr) => {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let minIndex = i;
+
+//     for (let k = i + 1; k < arr.length; k++) {
+//       if (arr[minIndex] > arr[k]) minIndex = k;
+//     }
+
+//     if (minIndex !== i) {
+//       [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+//     }
+//   }
+
+//   return arr;
+// };
+
+// console.log(selectionSort([6, 3, 1, 4, 2, 90, 10])); // [1, 2, 3, 4, 6, 10, 90]
+// // [1, 3, 6, 4, 2, 90, 10]
+// // [1, 2, 6, 4, 3, 90, 10]
+// // [1, 2, 3, 4, 6, 90, 10]
+// // [1, 2, 3, 4, 6, 10, 90]
+
 const selectionSort = (arr) => {
-  for (let i = 0; i < arr.length - 1; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let minIndex = i;
 
     for (let k = i + 1; k < arr.length; k++) {
@@ -7,15 +29,11 @@ const selectionSort = (arr) => {
     }
 
     if (minIndex !== i) {
-      [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
     }
   }
 
   return arr;
 };
 
-console.log(selectionSort([6, 3, 1, 4, 2, 90, 10])); // [1, 2, 3, 4, 6, 10, 90]
-// [1, 3, 6, 4, 2, 90, 10]
-// [1, 2, 6, 4, 3, 90, 10]
-// [1, 2, 3, 4, 6, 90, 10]
-// [1, 2, 3, 4, 6, 10, 90]
+console.log(selectionSort([36, 2, 1, 20, 15, 30, 80, 25])); // [1, 2, 15, 20, 25, 30, 36, 80]
